d9a9dc10690378a01f7424e96f9fde8d8d7dd5eb
laylark <laylan@gmail.com>
2022-11-24 15:42:31 -0600

    Refactor form using Django forms

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django import forms
 
 from . import util
 
+class EntryForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label="Content", widget=forms.Textarea)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -37,13 +41,19 @@ def search(request):
 def new(request):
     if request.method == "POST":
 
-        title = request.POST.get("title")
-        content = request.POST.get("content")
+        form = EntryForm(request.POST)
+
+        if not form.is_valid():
+            return render(request, "encyclopedia/new.html", {"form": form})
+
+        title = form.cleaned_data["title"]
+        content = form.cleaned_data["content"]
 
         for entry in util.list_entries():
             if title == entry:
                 messages.error(request, "Error. Page already exists.")
-                return render(request, "encyclopedia/new.html")
+                return render(request, "encyclopedia/new.html", {"form": form})
         util.save_entry(title, content)
         return redirect("entry", title=title)
-    return render(request, "encyclopedia/new.html")
+
+    return render(request, "encyclopedia/new.html", {"form": EntryForm()})
